e9c33c8f9645692a49fde0cb8d174530d0e6c5ad
franr <franreke@gmail.com>
2025-02-09 13:13:41 +0100

    feat: Enhance admin game menu with navigation drawer and UI improvements
    This commit enhances the administrator's game menu by introducing a navigation drawer and improving the user interface.
    
    -   Added a navigation drawer to `MenuDelJuegoAdministradorActivity` using `ModalNavigationDrawer` and `ModalDrawerSheet`.
    -   Implemented the functionality to open and close the drawer using an `ExtendedFloatingActionButton`.
    -   Replaced the default icon in the floating action button with a custom "menus" icon.
    -   Added a custom icon file `menus.png`.
    - Updated the `MenuDelAdministrador` composable to include the drawer and the floating action button.
    -   Updated colors used in the drawer and background of `MenuDelJuegoAdministradorActivity`.
    - Updated the Floating Action Button with a new size.
    - Updated the text of the `Scaffold` to display `menu del juego`.

--- app/src/main/java/com/example/projectofinalfranciscocompose/MenuDelJuegoAdministradorActivity.kt
+++ app/src/main/java/com/example/projectofinalfranciscocompose/MenuDelJuegoAdministradorActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 //import para poder alinear el contenido del Scafold
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.DrawerValue
@@ -26,7 +28,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,53 +43,57 @@ class MenuDelJuegoAdministradorActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProjectoFinalFranciscoComposeTheme {
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                val scope = rememberCoroutineScope()
-
-                ModalNavigationDrawer(
-                    drawerState = drawerState,
-                    drawerContent = {
-                        ModalDrawerSheet(
-                            modifier = Modifier
-                                    .background(colorResource(R.color.gray))
-                                .padding(16.dp),
-                            //Cambiar el color del fondo del drawer
-                            drawerContainerColor = colorResource(R.color.black)
-                        ) {
-                            // Drawer content
-                        }
-                    },
-                    modifier = Modifier.background(colorResource(R.color.fondo))
-                ) {
-                    Scaffold(
-                        floatingActionButton = {
-                            ExtendedFloatingActionButton(
-                                text = { Text("Show drawer") },
-                                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                                onClick = {
-                                    scope.launch {
-                                        if (drawerState.isClosed) {
-                                            drawerState.open()
-                                        } else {
-                                            drawerState.close()
-                                        }
-                                    }
-                                }
-                            )
-                        }
-                    ) { contentPadding ->
-                        Box(modifier = Modifier.fillMaxSize().background(colorResource(R.color.fondo))) {
-                            MenuDelAdministrador(modifier = Modifier.padding(contentPadding))
-                        }
-                    }
-                }
+                MenuDelAdministrador()
             }
         }
     }
 }
 
 @Composable
-fun MenuDelAdministrador( modifier: Modifier = Modifier) {}
+fun MenuDelAdministrador( modifier: Modifier = Modifier) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier
+                    .background(colorResource(R.color.gray))
+                    .padding(16.dp),
+                //Cambiar el color del fondo del drawer
+                drawerContainerColor = colorResource(R.color.black)
+            ) {
+                // Drawer content
+            }
+        },
+        modifier = Modifier.background(colorResource(R.color.fondo))
+    ) {
+        Scaffold(
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = {},
+                    icon = { Icon(ImageBitmap.imageResource(R.drawable.menus), contentDescription = " ", modifier = Modifier.padding(10.dp)) },
+                    onClick = {
+                        scope.launch {
+                            if (drawerState.isClosed) {
+                                drawerState.open()
+                            } else {
+                                drawerState.close()
+                            }
+                        }
+                    }
+                    ,modifier = Modifier.width(100.dp))
+            }
+        ) { contentPadding ->
+            Text(
+                text = "menu del juego",modifier=Modifier.padding(contentPadding)
+            )
+        }
+    }
+
+
+}
 
 
 
@@ -94,45 +102,6 @@ fun MenuDelAdministrador( modifier: Modifier = Modifier) {}
 @Composable
 fun GreetingPreview4() {
     ProjectoFinalFranciscoComposeTheme {
-        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-        val scope = rememberCoroutineScope()
-
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                ModalDrawerSheet(
-                    modifier = Modifier
-                        .background(colorResource(R.color.gray))
-                        .padding(16.dp),
-                    //Cambiar el color del fondo del drawer
-                    drawerContainerColor = colorResource(R.color.black)
-                ) {
-                    // Drawer content
-                }
-            },
-            modifier = Modifier.background(colorResource(R.color.fondo))
-        ) {
-            Scaffold(
-                floatingActionButton = {
-                    ExtendedFloatingActionButton(
-                        text = {},
-                        icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                        onClick = {
-                            scope.launch {
-                                if (drawerState.isClosed) {
-                                    drawerState.open()
-                                } else {
-                                    drawerState.close()
-                                }
-                            }
-                        }
-                    )
-                }
-            ) { contentPadding ->
-                Box(modifier = Modifier.fillMaxSize().background(colorResource(R.color.fondo))) {
-                    MenuDelAdministrador(modifier = Modifier.padding(contentPadding))
-                }
-            }
-        }
+        MenuDelAdministrador()
     }
 }
